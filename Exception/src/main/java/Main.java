e8a3d6f5b6eb1953f7285238c5e51ce81b348969
svetlana_klimova <sfettkoo@mail.ru>
2023-09-27 09:08:40 +0300

    done

--- Exception/src/main/java/Main.java
+++ Exception/src/main/java/Main.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static ArrayList<Integer> arrayList = new ArrayList();
+    static ArrayList<Integer> arrayList = new ArrayList<>();
 
     public static void main(String[] args) throws MyException {
+        System.out.println("Введите число: ");
         Scanner scanner = new Scanner(System.in);
         int count;
         try {
--- Exception/src/main/java/MyException.java
+++ Exception/src/main/java/MyException.java
@@ -1,14 +1,18 @@
 public class MyException extends Exception{
 
     public MyException(){
+        getSuppressed();
     }
 
     public MyException(String message) { //сообщение
         super(message);
+        printStackTrace();
     }
 
     public MyException(String message, Throwable cause) { // сообщение с причиной
         super(message, cause);
+        fillInStackTrace();
+        getLocalizedMessage();
     }
 
     public MyException(Throwable cause) { // причина
